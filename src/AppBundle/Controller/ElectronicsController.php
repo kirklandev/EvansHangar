e4b9538d003ebbdb7fc3d0c70a0f53a22c4e2da4
Evan Kirkland <ekirkland@credera.com>
2016-08-19 08:05:02 -0500

    Updated carousel and form page.

---
+++ src/AppBundle/Controller/ElectronicsController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ekirkland
+ * Date: 8/19/16
+ * Time: 8:04 AM
+ */
+
+namespace AppBundle\Controller;
+
+
+class ElectronicsController
+{
+
+}
---
+++ src/AppBundle/Controller/QuadcopterController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ekirkland
+ * Date: 8/19/16
+ * Time: 7:57 AM
+ */
+
+namespace AppBundle\Controller;
+
+
+class QuadcopterController
+{
+
+}
